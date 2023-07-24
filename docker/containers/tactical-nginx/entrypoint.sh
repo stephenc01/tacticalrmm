436bd0d74189416fc7a2547502c1347ccf2ee4d6
Stephen <stephenc01@users.noreply.github.com>
2023-07-24 13:28:01 -0400

    fix action and versions

--- docker/containers/tactical-nginx/entrypoint.sh
+++ docker/containers/tactical-nginx/entrypoint.sh
@@ -16,6 +16,9 @@ set -e
 : "${CERT_PRIV_PATH:=${TACTICAL_DIR}/certs/privkey.pem}"
 : "${CERT_PUB_PATH:=${TACTICAL_DIR}/certs/fullchain.pem}"
 
+#make certificate directory
+mkdir -p /opt/tactical/certs/
+
 # remove default config
 rm -f /etc/nginx/conf.d/default.conf
 
